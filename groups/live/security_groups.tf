e469e34a03f9396914f915dabae40fcb5ddaf688
M Roberts <mroberts@companieshouse.gov.uk>
2022-03-16 11:34:11 +0000

    corrected some badly formed names and descriptions after a change of variable usage.
    changed instance type to try and address the compatibility issue with RHEL6 marketplace requirements

--- groups/live/security_groups.tf
+++ groups/live/security_groups.tf
@@ -2,8 +2,8 @@ module "jfil_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 3.0"
 
-  name        = "sgr-${var.category}"
-  description = "Security group for the ${var.category} ${var.environment} Server"
+  name        = "sgr-${var.jfil_ec2_name}"
+  description = "Security group for the ${var.category} ${var.environment} ${var.jfil_ec2_name} Server"
   vpc_id      = data.aws_vpc.vpc.id
 
   egress_rules = ["all-all"]
