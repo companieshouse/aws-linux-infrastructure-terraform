309a99245f599aa56687afa591b411534c38d015
W Watters <wwatters@companieshouse.gov.uk>
2022-03-30 10:59:27 +0100

    Linux Bulk Gateway Server

--- groups/staging/cloudwatch.tf
+++ groups/staging/cloudwatch.tf
@@ -1,19 +1,3 @@
-resource "aws_cloudwatch_log_group" "jfil" {
-  for_each = local.jfil_cw_logs
-
-  name              = each.value["log_group_name"]
-  retention_in_days = lookup(each.value, "log_group_retention", var.default_log_group_retention_in_days)
-  kms_key_id        = lookup(each.value, "kms_key_id", data.aws_kms_key.logs.arn)
-
-  tags = merge(
-    local.default_tags,
-    map(
-      "Name", "${var.category}-jfil-server",
-      "ServiceTeam", var.ServiceTeam
-    )
-  )
-}
-
 resource "aws_cloudwatch_log_group" "bulk_gateway" {
   for_each = local.bulk_gateway_cw_logs
 
--- groups/staging/security_groups.tf
+++ groups/staging/security_groups.tf
@@ -1,14 +1,3 @@
-module "jfil_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "~> 3.0"
-
-  name        = "sgr-${var.jfil_ec2_name}"
-  description = "Security group for the ${var.category} ${var.environment} ${var.jfil_ec2_name} Server"
-  vpc_id      = data.aws_vpc.vpc.id
-
-  egress_rules = ["all-all"]
-}
-
 module "bulk_gateway_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 3.0"
